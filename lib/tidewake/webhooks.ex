1d7c61d7127b444c6560ce467c5af85484d190fd
CarlosCaetano <carlos.caetano@attrus.com>
2026-09-07 07:28:37 -0300

    feat: add event context operations

--- lib/tidewake/webhooks.ex
+++ lib/tidewake/webhooks.ex
@@ -1,10 +1,24 @@
 defmodule Tidewake.Webhooks do
   @moduledoc """
-  Manages webhook destination endpoints and their persistence.
+  Manages webhook events, destination endpoints, and their persistence.
   """
 
   alias Tidewake.Repo
-  alias Tidewake.Webhooks.Endpoint
+  alias Tidewake.Webhooks.{Endpoint, Event}
+
+  def create_event(attrs) do
+    %Event{}
+    |> Event.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def get_event(id) do
+    Repo.get(Event, id)
+  end
+
+  def get_event_by_external_id(external_id) do
+    Repo.get_by(Event, external_id: external_id)
+  end
 
   def list_endpoints do
     Repo.all(Endpoint)
