37792dbbb4828605aa9587061bf34063e4686f1f
CarlosCaetano <carlos.caetano@attrus.com>
2026-09-09 00:52:25 -0300

    feat: add delivery context operations

--- lib/tidewake/webhooks.ex
+++ lib/tidewake/webhooks.ex
@@ -4,7 +4,7 @@ defmodule Tidewake.Webhooks do
   """
 
   alias Tidewake.Repo
-  alias Tidewake.Webhooks.{Endpoint, Event}
+  alias Tidewake.Webhooks.{Delivery, Endpoint, Event}
 
   def create_event(attrs) do
     %Event{}
@@ -18,6 +18,24 @@ defmodule Tidewake.Webhooks do
 
   def get_event_by_external_id(external_id) do
     Repo.get_by(Event, external_id: external_id)
+  end
+
+  def create_delivery(%Event{} = event, %Endpoint{active: true} = endpoint) do
+    %Delivery{event_id: event.id, endpoint_id: endpoint.id}
+    |> Delivery.changeset(%{})
+    |> Repo.insert()
+  end
+
+  def create_delivery(%Event{}, %Endpoint{active: false}) do
+    {:error, :endpoint_inactive}
+  end
+
+  def get_delivery(id) do
+    Repo.get(Delivery, id)
+  end
+
+  def get_delivery_by_event_and_endpoint(%Event{} = event, %Endpoint{} = endpoint) do
+    Repo.get_by(Delivery, event_id: event.id, endpoint_id: endpoint.id)
   end
 
   def list_endpoints do
